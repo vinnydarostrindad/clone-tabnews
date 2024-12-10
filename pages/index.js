d40f369996225389a87e62641e457c65e48aef22
Vinny Daros Trindade <122296481+vinnydarostrindad@users.noreply.github.com>
2024-12-10 16:45:28 +0000

    adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home () {
+    return <h1>Obrigado por fazer parte da minha vida. Espero que você possa continuar fazendo parte dela, te amo!<br/> Nunca esqueça que Jesus te ama também! E bem mais do que eu.</h1>
+}
+
+export default Home;
