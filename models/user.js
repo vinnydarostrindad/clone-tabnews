8a603c08faa606e0e7577178fa05b053176c3742
Vinny Daros Trindade <122296481+vinnydarostrindad@users.noreply.github.com>
2025-05-08 17:55:52 +0000

    feat: create `user.findOneByUsername` method and `/api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 import database from "infra/database";
-import { ValidationError } from "infra/errors.js";
+import { ValidationError, NotFoundError } from "infra/errors.js";
 
 async function create(userInputValues) {
   await validateUniqueUsername(userInputValues.username);
@@ -58,7 +58,6 @@ async function create(userInputValues) {
       values: [username],
     });
 
-    console.log(result);
     if (result.rowCount > 0) {
       throw new ValidationError({
         message: "O username informado ja está sendo utilizado.",
@@ -68,8 +67,39 @@ async function create(userInputValues) {
   }
 }
 
+async function findOneByUsername(username) {
+  const user = await runSelectQuery(username);
+  return user;
+
+  async function runSelectQuery(username) {
+    const result = await database.query({
+      text: `
+        SELECT
+          *
+        FROM
+          users
+        WHERE
+          LOWER(username) = LOWER($1)
+        LIMIT
+          1
+      ;`,
+      values: [username],
+    });
+
+    if (result.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O username informado não foi encontrado no sistema.",
+        action: "Verifique se o username está digitado corretamente.",
+      });
+    }
+
+    return result.rows[0];
+  }
+}
+
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
