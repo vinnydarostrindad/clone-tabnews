deb212a0ccad9e8b1b6ce86f4cdf8aeb2e477c63
Vinny Daros Trindade <122296481+vinnydarostrindad@users.noreply.github.com>
2025-05-10 15:34:13 +0000

    feat: hash `password` in `user.create`

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
-import database from "infra/database";
+import password from "models/password.js";
+import database from "infra/database.js";
 import { ValidationError, NotFoundError } from "infra/errors.js";
 
 async function create(userInputValues) {
   await validateUniqueUsername(userInputValues.username);
   await validateUniqueEmail(userInputValues.email);
+  await hashPasswordInObject(userInputValues);
 
   const newUser = await runInsertQuery(userInputValues);
   return newUser;
@@ -64,6 +66,11 @@ async function create(userInputValues) {
         action: "Utilize outro username para realizar o cadastro.",
       });
     }
+  }
+
+  async function hashPasswordInObject(userInputValues) {
+    const hashedPassword = await password.hash(userInputValues);
+    userInputValues.password = hashedPassword;
   }
 }
 
--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,5 +1,7 @@
 import { version as uuidVersion } from "uuid";
 import orchestrator from "tests/orchestrator.js";
+import user from "models/user.js";
+import password from "models/password.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -30,7 +32,7 @@ describe("POST /api/v1/users", () => {
         id: responseBody.id,
         username: "Vinny Daros Trindade",
         email: "izaisaspereira@gmail.com",
-        password: "senha123",
+        password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
@@ -38,6 +40,21 @@ describe("POST /api/v1/users", () => {
       expect(uuidVersion(responseBody.id)).toBe(4);
       expect(Date.parse(responseBody.created_at)).not.toBeNull();
       expect(Date.parse(responseBody.updated_at)).not.toBeNull();
+
+      const userInDatabease = await user.findOneByUsername(
+        "Vinny Daros Trindade",
+      );
+      const correctPasswordMatch = await password.compare(
+        "senha123",
+        userInDatabease.password,
+      );
+      const incorrectPasswordMatch = await password.compare(
+        "SenhaErrada",
+        userInDatabease.password,
+      );
+
+      expect(correctPasswordMatch).toBe(true);
+      expect(incorrectPasswordMatch).toBe(false);
     });
     test("With duplicated email", async () => {
       const response1 = await fetch("http://localhost:3000/api/v1/users", {
